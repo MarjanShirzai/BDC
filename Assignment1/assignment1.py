1ddfd201442ea3df9d640b1dc61375e4e74e0ec0
Marjan Shirzai <mshirzai@bioinf.nl>
2023-05-24 11:32:45 +0200

    changes made to meet pylint score

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -22,37 +22,43 @@ import os
 # Defining an output queue
 output = mp.Queue()
 
-def phredScore(inputfile, output,  startSize, endSize ):
+def phred_score(inputfile, output, start_size, end_size):
+    """
+    Calculating phred score
+    """
     # Open the given input file
     with open(inputfile, "r") as fastq:
-        fastq.seek(startSize)
-        phredscore = []
+        fastq.seek(start_size)
+        phred_score = []
         counters = []
 
         # The tell() method returns the current file position in a file stream.
-        while fastq.tell() <= endSize:
+        while fastq.tell() <= end_size:
             line = fastq.readline()
             if line == "":
                 break
             if line.startswith("+"):
-                fileLines = fastq.readline()
-                qualityString = [ord(character) - 33 for character in fileLines[:-1]]
-                phredScoreLen = len(qualityString)
+                file_lines = fastq.readline()
+                quality_string = [ord(character) - 33 for character in file_lines[:-1]]
+                phred_score_len = len(quality_string)
 
                 # Adding zero(s) to the lists is the lists are not the same length
-                for phred in range(phredScoreLen):
+                for phred in range(phred_score_len):
                     try:
-                        if len(phredscore) < len(qualityString):
-                            phredscore[phred] = 0
+                        if len(phred_score) < len(quality_string):
+                            phred_score[phred] = 0
                             counters[phred] = 0
                     except IndexError:
-                        phredscore.append(0)
+                        phred_score.append(0)
                         counters.append(0)
-                    phredscore[phred] += qualityString[phred]
+                    phred_score[phred] += quality_string[phred]
                     counters[phred] += 1
-        output.put([phredscore, counters])
+        output.put([phred_score, counters])
 
 def main():
+    """
+    Arguments are passed and results are processed in the main.
+    """
     parser = argparse.ArgumentParser(description='Process FastQ file to produce average PHRED scores per base')
     parser.add_argument("fastqFile", help= "The fastq file", nargs="*")
     parser.add_argument("-o", "--output",help="Directs the output to the given output name", nargs="*")
@@ -64,20 +70,20 @@ def main():
         processes = []
 
         # Creating chunks
-        fileInformation = os.stat(files)
-        chunks = round(fileInformation.st_size / args.cores)
+        file_information = os.stat(files)
+        chunks = round(file_information.st_size / args.cores)
 
-        startPosition = 0
-        endPosition = chunks
+        start_position = 0
+        end_position = chunks
 
         # creating the processes
-        for processNumber in range(0, args.cores):
+        for process_number in range(0, args.cores):
             print("Process started")
-            processRange = mp.Process(target=phredScore, args=(files, output, startPosition, endPosition))
-            processes.append(processRange)
-            processRange.start()
-            startPosition += chunks
-            endPosition += chunks
+            process_range = mp.Process(target=phred_score, args=(files, output, start_position, end_position))
+            processes.append(process_range)
+            process_range.start()
+            start_position += chunks
+            end_position += chunks
 
         numbers = []
         counters = []
@@ -93,17 +99,17 @@ def main():
         results = [sum(x) for x in zip(*numbers)]
         counts = [sum(x) for x in zip(*counters)]
 
-        for av in range(0, len(results)):
-            num = results[av] / counts[av]
+        for average_num in range(0, len(results)):
+            num = results[average_num] / counts[average_num]
             average.append(num)
 
         # If the output file is given as argument than print the results to the output file
         if args.output:
-            with open(args.output[file], "w") as myFile:
-                toWrite = csv.writer(myFile)
-                toWrite.writerow(["Base nr","Average PHRED value"])
+            with open(args.output[count], "w") as my_file:
+                to_write = csv.writer(my_file)
+                to_write.writerow(["Base nr","Average PHRED value"])
                 for index, score in enumerate(average):
-                    toWrite.writerow([index, score])
+                    to_write.writerow([index, score])
         else:
             print("{}".format(files))
             print(average)
