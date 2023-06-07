38de2a171c8a4cc42205bfb68152eb53755ce392
Marjan Shirzai <mshirzai@bioinf.nl>
2023-06-07 13:01:16 +0200

    source to conda/ output to testOutput

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source ~/../../commons/conda/conda_load.sh
+
 #SBATCH --time 2:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --nodes=1  
 #SBATCH --mem=4000  
 
-python3 assignment3.py  -n 2 fastqfile.fastq -o results.csv
+python3 assignment3.py -n 2 fastqfile.fastq > testOutput.txt
