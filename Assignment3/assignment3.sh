f2fd03402277f0e7c74fd6c4b565c2b9a7f52743
Marjan Shirzai <mshirzai@nuc222.bin.bioinf.nl>
2024-07-23 08:34:27 +0200

    Small adjustments

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -2,13 +2,14 @@
 
 
 #SBATCH --time 2:00:00
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=4
 #SBATCH --nodes=2  
 #SBATCH --mem=4000  
 #SBATCH --output=output.csv
 #SBATCH --partition=short
 
 #module load python/3.9-2022.05 # load python
+output="output.csv"
+cores=4
 
-
-parallel -j 2 python3 assignment3.py -j 2 ::: fastqfile.fastq > $output
+parallel -j $cores  python3 assignment3.py -n $cores ::: fastqfile.fastq fastqfile2.fastq > $output
